5d143b3490dac04cf77073197cee88ee7ae6c9a6
coenraadv <coenraad@originfintech.io>
2024-06-14 09:14:03 +0200

    more modifications

--- install_nvim.sh
+++ install_nvim.sh
@@ -2,7 +2,7 @@
 
 NEOVIM_REPO='https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz'
 NVCHAD_REPO='https://github.com/NvChad/starter'
-CONFIG_REPO=https://raw.githubusercontent.com/coenraadv/nvim_config/main/nvim
+CONFIG_REPO=i'https://raw.githubusercontent.com/coenraadv/nvim_config/main/nvim'
 
 function error_exit {
     echo "$1" 1>&2
@@ -18,14 +18,16 @@ sudo rm -rf /opt/nvim || error_exit "Failed to remove existing Neovim installati
 echo "Extracting Neovim..."
 sudo tar -C /opt -xzf nvim-linux64.tar.gz || error_exit "Failed to extract Neovim."
 
-echo "Adding Config to NvChad"
-curl -o ~/.config/nvim $CONFIG_REPO || error_exit "Failed to add config to NvChad"
-
 echo "Adding path to bashrc"
 echo 'export PATH="$PATH:/opt/nvim-linux64/bin"' >> ~/.bashrc || error_exit "Failed to add path to bash_rc"
 
 echo "Reloading bashrc"
 source ~/.bashrc || error_exit "Failed to reload bashrc"
 
-echo "Neovim installation complete."
-nvim || error_exit "Failed to start nvim"
+echo "Cloning NvChad repository..."
+sudo rm -r ~/.config/nvim
+git clone $NVCHAD_REPO ~/.config/nvim && nvim || error_exit "Failed to clone NvChad repository"
+
+echo "Adding Config to NvChad"
+sudo rm -r ~/.config/nvim
+curl -o ~/.config/nvim $CONFIG_REPO || error_exit "Failed to add config to NvChad"
